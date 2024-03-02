7c5bbee1f91c68b31e78689d91352887835677d1
vaibhavyxx <vd5443@rit.edu>
2024-03-02 17:28:23 -0500

    buildings class to transition scenes

--- Website/classes.js
+++ Website/classes.js
@@ -1,10 +1,16 @@
 class Building extends PIXI.Sprite{
-    constructor(x,y, tex, scaleX, scaleY,scene){
+    constructor(x,y, tex, scaleX, scaleY,current, next){
       super(tex);
       this.x=x;
       this.y =y;
       this.scale.x = scaleX;
       this.scale.y = scaleY
       scene.addChild(this);
+      this.interactive =true;
+
+      this.on('click', function(){
+        current.visible = false;
+        next.visible = true;
+      })
     }
   }
