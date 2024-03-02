80f0501b549e2551142023338e8fbb0b54bcc760
vaibhavyxx <vd5443@rit.edu>
2024-03-02 17:33:55 -0500

    for scenes

--- Website/js/main.js
+++ Website/js/main.js
@@ -1,4 +1,14 @@
 "use strict";
+//scenes
+let work;
+let bankA;
+let bankB;
+let home;
+let IRS;
+let w2Scene;
+let fin;    
+let menu;   
+
 const app = new PIXI.Application({
     width:1024,
     height:768,
