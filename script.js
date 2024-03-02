d14326cc99fae2f7e0a8d22326e0f4fe16ce983d
Lisa Pham <lvp1731@rit.edu>
2024-03-02 17:11:43 -0500

    switch js

--- script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const app = new PIXI.Application({
-    width:1024,
-    height:768,
-    backgroundColor: 0xfabedc,
-});
-let game = document.getElementById('game');
-game.appendChild(app.view);
